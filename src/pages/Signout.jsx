d646809e7bfb28b484cb4b5bd7efa346302c3888
priyankanaidu <npriyanka783@gmail.com>
2025-02-12 14:04:53 +0530

    [Btns]

---
+++ src/pages/Signout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Signout() {
+    return (
+        <>
+        <button>Sign up</button>
+        <button>Forget Password</button>
+        </>
+    )
+}
+
+export default Signout
