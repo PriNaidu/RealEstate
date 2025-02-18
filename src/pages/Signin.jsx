f5c4296e267d09e13b08620720aed0ccbbc7864b
priyankanaidu <npriyanka783@gmail.com>
2025-02-18 13:27:46 +0530

    [Fix]

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,17 +1,18 @@
-import React from 'react'
+import React from "react";
 
 function Signin() {
-    return (
-        <>
-     <form>
-      <div className="flex">
-      <input type="text" placeholder='Please enter email'/>
-      <input type="text" placeholder='Please enter password'/>
-      </div>
-
-     </form>
-        </>
-    )
+  return (
+    <>
+      <form>
+        <div className="flex">
+          <input type="text" placeholder="Please enter email" />
+          <input type="text" placeholder="Please enter password" />
+          <button>Sign in</button>
+          <button> Forgot Password</button>
+        </div>
+      </form>
+    </>
+  );
 }
 
-export default Signin
+export default Signin;
